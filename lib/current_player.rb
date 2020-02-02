d38b5368e2b38396b6247534b63dbe495a2ec4f8
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-02 18:29:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,11 @@
-require "pry"
-
 def turn_count(board)
   counters = 0
   board.each do |x|
     if x == "X" || x=="O"
       counters+=1
       puts counters
-binding.pry
     end
   return counters
-
   end
 end
 
