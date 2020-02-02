2ff327498de9fbbb60eac6df71717b936bd2a261
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-02 18:37:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,11 @@
 def turn_count(counts)
   counters = 0
-  counts.each do |x|
-    if x == "X" || x=="O"
-      counters+=1
+  counts.each do |i|
+    if i == "X" || i== "O"
+      counters +=1
     end
-  return counters
   end
 end
-
-
 #def current_player()
 
 #end
